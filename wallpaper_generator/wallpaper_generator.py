7bdb5d244305b0cab22296e772bee92c1f89d42a
Cynthia7979 <CynthiaWang7979@hotmail.com>
2018-12-04 13:09:24 -0500

    Multiple changes, but nothing done

---
+++ wallpaper_generator/wallpaper_generator.py
@@ -0,0 +1,13 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+pygame.display.init()
+pygame.display.set_mode((600, 400))
+pygame.display.set_caption('Wallpaper Generator v0.1')
+
+
+def main():
+    print('Program running (where did I get this habit???)')
