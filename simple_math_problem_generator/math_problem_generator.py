5587eb70aabab32560700b5823042bd462b0b8e6
Cynthia7979 <CynthiaWang7979@hotmail.com>
2020-05-28 21:18:34 +0800

    math problem generator. No new problem types, just debugging

--- simple_math_problem_generator/math_problem_generator.py
+++ simple_math_problem_generator/math_problem_generator.py
@@ -3,17 +3,17 @@ import sys, os
 import json
 from sympy.abc import *
 from sympy import *
+from sympy.core.sympify import SympifyError
 
 
 class Problem(object):
     """
-    Base class for single math problems
+    Class for single math problems
     """
     def __init__(self, question, answer=None):
         """
-
-        :param problem:
-        :param answer: None
+        :param question: Question represented as SymPy expression
+        :param answer: Answer represented either as SymPy expression or int/float value
         """
         self.question = question
         self.answer = answer
@@ -27,7 +27,9 @@ class Problem(object):
 
 def main():
     global type_config, global_config
+    # Get command line arguments, if any
     cli_args = sys.argv[1:]
+    # Load config files
     type_weights = list(load_configuration_file('./config/weights.json').values())
     type_config = load_configuration_file('./config/problems.json')
     global_config = load_configuration_file('./config/config.json')
@@ -35,21 +37,25 @@ def main():
     init_printing()
 
     if cli_args:  # Output (command line) mode
-        problem_output_format, answer_output_format = list(json.load('./config/output.json').values())
-        num = int(cli_args[1])
+        num = int(cli_args[0])
         if len(cli_args) > 1:
-            output_path = cli_args[2]
+            output_path = cli_args[1]
         else:
             output_path = './output/'
-        f_problem = open(os.path.join(output_path, 'problems.txt'), 'w')
-        f_answer = open(os.path.join(output_path, 'answers.txt'), 'w')
+
+        with open('./config/output.json') as f:
+            problem_output_format, answer_output_format = list(json.load(f).values())
+        f_problem = open(os.path.join(output_path, 'problems.txt'), 'w', encoding='utf-8')
+        f_answer = open(os.path.join(output_path, 'answers.txt'), 'w', encoding='utf-8')
         for i in range(num):
-            q_type = random.choices(QUESTION_TYPES, weights=type_weights)
+            q_type = random.choices(QUESTION_TYPES, weights=type_weights)[0]
             problem = q_type()
-            f_problem.write(problem_output_format.format(question=problem.question, no=i+1))
-            f_answer.write(answer_output_format.format(answer=str(problem.answer), no=i+1))
-        f_problem.close()
-        f_answer.close()
+            if global_config['pretty_output']:
+                f_problem.write(problem_output_format.format(content=pretty(problem.question), no=i+1))
+                f_answer.write(answer_output_format.format(content=pretty(problem.answer), no=i+1))
+            else:
+                f_problem.write(problem_output_format.format(content=str(problem.question), no=i+1))
+                f_answer.write(answer_output_format.format(content=str(problem.answer), no=i+1))
     else:  # Interactive (shell) mode
         while True:
             q_type = random.choices(QUESTION_TYPES, weights=type_weights)[0]
@@ -61,15 +67,17 @@ def main():
                 break
 
             answer_is_right = False
-            if users_answer and users_answer.strip():
+            if users_answer and users_answer.strip():  # Not empty string
                 try:
-                    users_answer = sympify(users_answer)
+                    users_answer = sympify(users_answer)  # Convert to SymPy Expression
                     if users_answer == problem.answer:
                         answer_is_right = True
-                except Exception as e:
-                    print('Your input can\'t be recognized, attempting the workaround instead...')
-                    if users_answer == str(problem.answer):
+                except SympifyError:  # If answer can't be converted
+                    print(f'Your input can\'t be recognized, attempting the workaround instead...')
+                    if users_answer == str(problem.answer):  # Then convert the answer to str
                         answer_is_right = True
+            else:
+                answer_is_right = False  # Just to be clear..
             if answer_is_right:
                 print('Correct.')
                 print('-'*20)
@@ -122,9 +130,13 @@ def random_frac(interval, denom_interval):
 
 
 def simple_computation():
+    """
+    Simple 4-operand computations
+    :return: Problem object
+    """
     func_config = combine_configurations(type_config['simple_computation'], global_config)
     number_of_terms = random.randint(2, global_config['maximum_terms'])
-    question = None
+
     # first_term
     type_of_term = random.choice(VALUE_TYPES)
     if type_of_term == 'int':
@@ -133,6 +145,9 @@ def simple_computation():
         first_term = random_float(func_config["interval"], func_config["float_precision"])
     # elif type_of_term == 'frac':
     #     first_term = random_frac(func_config["interval"], func_config["denominator_interval"])
+    else:
+        raise ValueError(f'Unexpected type of term: {type_of_term}')
+
     question = str(first_term)
     for i in range(number_of_terms):
         type_of_term = random.choice(VALUE_TYPES)
@@ -142,6 +157,9 @@ def simple_computation():
             term = random_float(func_config["interval"], func_config["float_precision"])
         # elif type_of_term == 'frac':
         #     term = random_frac(func_config["interval"], func_config["denominator_interval"])
+        else:
+            raise ValueError(f'Unexpected type of term: {type_of_term}')
+
         operation = random.choice(['+', '-', '*', '/'])
         question += operation + str(term)
     answer = sympify(question).round(func_config['float_precision'])
@@ -149,8 +167,11 @@ def simple_computation():
     problem = Problem(question, answer)
     return problem
 
+
+#  Global constants
 QUESTION_TYPES = [simple_computation]
-VALUE_TYPES = ['int', 'float', #'frac'
+VALUE_TYPES = [
+    'int', 'float', #'frac'
                 ]
 
 
