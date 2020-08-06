3f5581de8d314832032742907b41978fc77a4868
Cynthia7979 <CynthiaWang7979@hotmail.com>
2020-08-06 10:36:44 +0800

    Random chooser: Custom source file and automatically switch to peuso-random.

--- random_chooser/random_chooser.py
+++ random_chooser/random_chooser.py
@@ -1,11 +1,11 @@
 import requests
+import random
 import sys, os
 
 
 def main(file='source.txt', debug=False):
     f = open(file, encoding='utf-8')
     lines = [s.strip('\n') for s in f.readlines()]
-    # choices = {}
     choices = []
     for i, line in enumerate(lines):
         splitted = line.split()
@@ -19,7 +19,11 @@ def main(file='source.txt', debug=False):
             frequency = 1
         choices.extend([choice]*frequency)
     number_of_choices = len(choices)-1
-    final_result = get_random_number(0, number_of_choices)
+    try:
+        final_result = get_random_number(0, number_of_choices)
+    except requests.exceptions.ConnectionError:
+        print('Cannot connect to RANDOM.ORG. Using pseudo-random instead...')
+        final_result = random.randint(0,number_of_choices)
     print(choices[final_result])
     if debug: print(choices)
 
@@ -30,4 +34,10 @@ def get_random_number(minimum, maximum, number_of_numbers=1, base=10):
 
 
 if __name__ == '__main__':
-    main(debug=True)
+    try:
+        _, file_, *debug_ = sys.argv
+        debug_ = False if debug_ == ['False'] else True
+    except ValueError:
+        file_ = 'source.txt'
+        debug_ = False
+    main(file=file_, debug=debug_)
