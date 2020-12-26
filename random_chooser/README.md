c0d50bf4e9dc2ddf06de43f0e155fd13c4245dfb
Cynthia7979 <CynthiaWang7979@hotmail.com>
2020-12-26 19:01:36 +0800

    Refined random_chooser

--- random_chooser/README.md
+++ random_chooser/README.md
@@ -2,24 +2,34 @@
 A simple program that uses [RANDOM.ORG](http://random.org)'s API to generate
 true random choices.
 
-**Note:** Repeated choices may appear.  
-If RANDOM.ORG is unavailable at the moment, the program will use `random.choice` to
+If RANDOM.ORG is unavailable at the moment, the program will use `random.randint` to
 generate pseudo-random choices.
 
-----
+## Command
 
-To customize, run the file with the following command in terminal:
+To customize, run the file with the following command:
 ```
-python random_chooser.py <number_of_choices> [file]
+python random_chooser.py [file=<filename>] [how_much=<how_much>] [repeated=(True|False)] [debug=(True|False)]
 ```
+`file`: Source file to get choices from. The choices **cannot** include spaces, and each choice have to be 
+written **on a separate line**. A choice can be "weighted" with a space and an integer following the choice. For example:
+```
+choice1
+choice2 12
+```
+adds **one** "choice1" and **twelve** "choice2" to the pool.
 
-`file` can be of any types, as long as it contains text.
+`how_much`: The number of choices to choose.
 
-For example, to generate 10 choices from `custom_file.abc`:
+`repeated`: Whether the choices are repeated or not. **Note:** Using weights will create repeated results in
+spite of this switch being set to `False`.
+
+`debug`: Changing this to `True` will let the program print the processed pool (with weights added) after 
+showing the results.
+
+## Example
+
+To generate 10 choices from `custom_file.abc` non-repeatedly:
 
 ```
-python random_chooser.py 10 custom_file.abc
-```
-
-Changing `debug_` at the end of `random_chooser.py` to `True` will let the program
-show the pool. This can be used to confirm if the file is read correctly.
+python random_chooser.py file=custom_file.abc repeated=False how_much=10
--- random_chooser/random_chooser.py
+++ random_chooser/random_chooser.py
@@ -3,7 +3,13 @@ import random
 import sys, os
 
 
-def main(file='source.txt', num=1, debug=False):
+def main(file='source.txt', how_much=1, repeated=True, debug=False, **overflow):
+    # Process argvs
+    how_much = int(how_much)
+    repeated = True if repeated=='True' else False
+    debug = True if debug=='True' else False
+
+    # Process choices and weights
     f = open(file, encoding='utf-8')
     lines = [s.strip('\n') for s in f.readlines()]
     choices = []
@@ -18,31 +24,57 @@ def main(file='source.txt', num=1, debug=False):
         else:
             frequency = 1
         choices.extend([choice]*frequency)
-    number_of_choices = len(choices)-1
-    final_result = []
-    for i in range(num):
+    sample_size = len(choices)-1
+    
+    # Get random numbers
+
+    result = []
+
+    if repeated:
         try:
-            final_result.append(get_random_number(0, number_of_choices))
+            result += get_random_numbers(0, sample_size, number_of_numbers=how_much)
         except requests.exceptions.ConnectionError:
-            print('Cannot connect to RANDOM.ORG. Using pseudo-random instead...')
-            final_result.append(random.randint(0,number_of_choices))
-    print('\n'.join([choices[random_index] for random_index in final_result]))
+            print('WARNING: Cannot connect to RANDOM.ORG. Using pseudo-randomness instead...')
+            result += [random.randint(0,sample_size) for i in range(how_much)]
+    else:
+        if how_much > sample_size:
+            print('WARNING: Number of numbers is larger than sample size. There WILL be repeated results.')
+            for i in range(how_much // sample_size):
+                try:
+                    result += get_random_sequence(0, sample_size, number_of_numbers=sample_size)
+                except requests.exceptions.ConnectionError:
+                    print('WARNING: Cannot connect to RANDOM.ORG. Using pseudo-randomness instead...')
+                    result += [random.randint(0,sample_size) for i in range(sample_size)]
+            how_much = how_much % sample_size
+        try:
+            result += get_random_sequence(0, sample_size, number_of_numbers=how_much)
+        except requests.exceptions.ConnectionError:
+            print('WARNING: Cannot connect to RANDOM.ORG. Using pseudo-randomness instead...')
+            result += [random.randint(0, sample_size) for i in range(how_much)]
+        
+    print('Results')
+    print('----------')
+    print('\n'.join([choices[int(random_index)] for random_index in result]))
     if debug: print('pool:', choices)
 
 
-def get_random_number(minimum, maximum, number_of_numbers=1, base=10):
-    random_number = int(requests.get(f"https://www.random.org/integers/?num={number_of_numbers}&min={minimum}&max={maximum}&col=5&base={base}&format=plain&rnd=new").text)
-    return random_number
+def get_random_numbers(minimum, maximum, number_of_numbers=1, base=10):
+    random_number = requests.get(f"https://www.random.org/integers/?num={number_of_numbers}&min={minimum}&max={maximum}&col=1&base={base}&format=plain&rnd=new").text
+    return random_number.split('\n')[:-1]
+
+
+def get_random_sequence(minimum, maximum, number_of_numbers=1):
+    # This function (and the URL below) is used to generate non-repeating numbers
+    random_sequence = requests.get(f'https://www.random.org/sequences/?min={minimum}&max={maximum}&col=1&format=plain&rnd=new').text
+    return random_sequence.split('\n')[:number_of_numbers]
 
 
 if __name__ == '__main__':
-    file_ = 'source.txt'
-    num_ = 1
-    debug_ = False
-    try:
-        _, num_, *file_ = sys.argv
-        num_ = int(num_)
-        file_ = file_[0] if file_ else 'source.txt'
-    except ValueError:
-        pass
-    main(file=file_, num=num_, debug=debug_)
+    kwargs = {'file': 'source.txt',
+              'how_much': 1,
+              'repeated': True,
+              'debug': False}
+    for argv in sys.argv[1:]:
+        key, value = argv.split('=')
+        kwargs[key] = value
+    main(**kwargs)
