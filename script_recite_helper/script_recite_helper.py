7f1e5265f40429d9a1565691d1d88de3af472319
Cynthia7979 <CynthiaWang7979@hotmail.com>
2020-12-26 14:56:33 +0800

    script reciter

---
+++ script_recite_helper/script_recite_helper.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+import os
+
+SPLITER = "："
+NAME_KEYWORD = "萍"
+FILENAME = './script.txt'
+
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+def main():
+    with open(FILENAME, encoding='utf-8') as f:
+        for line in f.readlines():
+            line = line.strip('\r\n')
+            speaker = line[:line.find(SPLITER)]
+            if NAME_KEYWORD in speaker:
+                _ = input('[YOUR LINE]')
+                delete_previous_line()
+                print(WARNING+line+ENDC)
+            else:
+                print(line)
+    os.system('cls' if os.name=='nt' else 'clear')
+    print('-- La fine --')
+
+
+def delete_previous_line():
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+
+
+if __name__ == "__main__":
+    main()
