184b494d204b7a05e1d88dc51cfed56ed1bf259e
Cynthia7979 <CynthiaWang7979@hotmail.com>
2021-04-03 18:11:03 +0800

    Mirror adult category pages - as a respond to recent events

--- scp_cn_mirror/scp_cn_mirror.py
+++ scp_cn_mirror/scp_cn_mirror.py
@@ -3,33 +3,21 @@ import sys, os
 import time
 from bs4 import BeautifulSoup
 
-mirror_dir = 'F://scp-wiki-cn/mirror/'
-source_dir = 'F://scp-wiki-cn/source/'
-home_page = "http://scp-wiki-cn.wikidot.com/tag-search/tag/%2b原创/limit/1617366553684/order/created_at%20desc/p/{p}"
-source_code_api = 'http://scp-wiki-cn.wikidot.com/ajax-module-connector.php'
-source_header = {
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Connection": "keep-alive",
-    "Content-Length": "123",
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Cookie": "__n_id2=2c0dd940-b761-4325-b623-24baf0df8dc7; __qca=P0-1315370410-1586302676763; __gads=ID=331c2923e1446493-22f4040a08c300d0:T=1597840740:RT=1597840740:R:S=ALNI_MbbzhxVublxJScJgOTVhxJewmkOmQ; _pbjs_userid_consent_data=6683316680106290; na-unifiedid=%7B%22TDID%22%3A%22d25825eb-3c34-46dc-922f-6bddad92a330%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222021-02-08T03%3A02%3A58%22%7D; WIKIDOT_SESSION_ID=1615981396_96062967; wikidot_udsession=1; wikidot_udsession=1; __utmz=1.1617262409.573.25.utmcsr=scpper.com|utmccn=(referral)|utmcmd=referral|utmcct=/user/3687969; wikidot_token7=8527b50858dd4c64aa0926fa47e33f41; __utma=1.533478953.1521295561.1617326732.1617366188.577; __utmc=1; __utmt=1; __utmt_old=1; __utmb=1.148.10.1617366188",
-    "Host": "scp-wiki-cn.wikidot.com",
-    "Origin": "http://scp-wiki-cn.wikidot.com",
-    "Referer": "http://scp-wiki-cn.wikidot.com/scp-cn-2552",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36 OPR/74.0.3911.218",
-    "X-Requested-With": "XMLHttpRequest",
+# mirror_dir = 'F://scp-wiki-cn/mirror/'
+mirror_dir = 'E://scp-wiki-cn/adult-mirror/'
+# home_page = "http://scp-wiki-cn.wikidot.com/tag-search/tag/%2b原创/limit/1617366553684/order/created_at%20desc/p/{p}"
+home_page = "http://scp-wiki-cn.wikidot.com/tag-search/tag/%2b原创/category/adult/limit/1617444176386/order/created_at%20desc"
+
+adult_headers = {
+    "Cookie": "__n_id2=2c0dd940-b761-4325-b623-24baf0df8dc7; __qca=P0-1315370410-1586302676763; __gads=ID=331c2923e1446493-22f4040a08c300d0:T=1597840740:RT=1597840740:R:S=ALNI_MbbzhxVublxJScJgOTVhxJewmkOmQ; _pbjs_userid_consent_data=6683316680106290; na-unifiedid=%7B%22TDID%22%3A%22d25825eb-3c34-46dc-922f-6bddad92a330%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222021-02-08T03%3A02%3A58%22%7D; WIKIDOT_SESSION_ID=1615981396_96062967; wikidot_udsession=1; wikidot_udsession=1; __utmz=1.1617262409.573.25.utmcsr=scpper.com|utmccn=(referral)|utmcmd=referral|utmcct=/user/3687969; wikidot_token7=4875975f575dc231c46977643726a30e; __utmc=1; __utma=1.533478953.1521295561.1617418092.1617443573.580; __utmt=1; __utmt_old=1; __utmb=1.20.10.1617443573"
 }
 
 
 def main():
     if not os.path.exists(mirror_dir):
         os.mkdir(mirror_dir)
-    if not os.path.exists(source_dir):
-        os.mkdir(source_dir)
 
-    for i in range(261, 273):
+    for i in range(1, 4):
         print('Iter', i)
         home_page_soup = get_soup(home_page.format(p=i))
         for link in get_links(home_page_soup):
@@ -64,9 +52,13 @@ def mirror(link):
     if os.path.exists(os.path.join(mirror_dir, page_name+'.html')):
         print('SKIPPING mirroring', page_name, 'due to existing file')
         return
-    print('Mirroring', page_name)
 
-    content = request(link)
+    if "/adult:" in link:
+        print("Mirroring", page_name, 'with category-specific method')
+        content = request(link+'/noredirect/true', headers=adult_headers)
+    else:
+        print('Mirroring', page_name)
+        content = request(link)
 
     content = str(content, encoding='utf-8')
     with open(os.path.join(mirror_dir, page_name+'.html'), 'w', encoding='utf-8') as f:
