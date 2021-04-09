8901291cb20e95ddaf124b2fad506901da79e79d
Cynthia7979 <CynthiaWang7979@hotmail.com>
2021-04-10 00:02:29 +0800

    Remove border feature

--- floating_text.py
+++ floating_text.py
@@ -61,41 +61,5 @@ class FloatingWindow(tk.Toplevel):
         self.geometry(f"+{x}+{y}")
 
 
-class FloatingWindow(tk.Toplevel):
-    def __init__(self, *args, **kwargs):
-        tk.Toplevel.__init__(self, *args, **kwargs)
-        self.overrideredirect(True)
-        self.x = 0
-        self.y = 0
-
-        self.canvas = tk.Canvas(self, bg=TRANSPARENT_COLOR, bd=1)
-        self.canvas.create_text(100, 100, font=(FONT_NAME, FONT_SIZE+2), text=TEXT, fill=BORDER_COLOR)
-        self.canvas.create_text(100, 100, font=(FONT_NAME, FONT_SIZE), text=TEXT, fill=TEXT_COLOR)
-        self.canvas.pack()
-
-        self.lift()
-        self.wm_attributes("-topmost", True)
-        self.wm_attributes("-transparentcolor", TRANSPARENT_COLOR)
-        self.wm_attributes('-alpha', ALPHA)
-
-        self.canvas.bind("<ButtonPress-1>", self.start_move)
-        self.canvas.bind("<ButtonRelease-1>", self.stop_move)
-        self.canvas.bind("<B1-Motion>", self.do_move)
-
-    def start_move(self, event):
-        self.x = event.x
-        self.y = event.y
-
-    def stop_move(self, event):
-        self.x = None
-        self.y = None
-
-    def do_move(self, event):
-        deltax = event.x - self.x
-        deltay = event.y - self.y
-        x = self.winfo_x() + deltax
-        y = self.winfo_y() + deltay
-        self.geometry(f"+{x}+{y}")
-
 app = App()
 app.floater.mainloop()
