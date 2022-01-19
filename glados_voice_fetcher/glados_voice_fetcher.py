8a38f7450c61f30c57b8891106b6b1c29595ce3b
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-01-19 11:53:19 +0800

    Basically done. I'd add multi-threaded support but that's too complicated so nevermind.

--- glados_voice_fetcher/glados_voice_fetcher.py
+++ glados_voice_fetcher/glados_voice_fetcher.py
@@ -11,22 +11,26 @@ def main():
         os.mkdir('./downloads/')
     logger = log.log_init(os.path.basename(__file__))
     logger.info('Starting...')
+    simplified_save = lambda text: fetch_and_save(text, get_safe_filename(f'{text}.wav', logger), logger)
 
     with open('script.txt', 'r') as f1:
         lines = f1.readlines()
         logger.info(f'Retrieved script from script.txt. {len(lines)} lines total.')
+
         for n, line in enumerate(lines):
-            line = line.strip('/n')
-            logger.info(f'Processing line {n+1} of {len(lines)}, '
-                        f'{len(lines)-n+1} ({((len(lines)-n+1)/len(lines)) * 100:.0f}%) left.')
-            safe_filename = get_safe_filename(f'{line}.wav', logger)
-            if os.path.exists(f'./downloads/{safe_filename}'):
-                logger.warning(f'Skipping due to existing file.')
+            line = line.replace('/n', '').replace('/r', '')
+            n = n+1
+
+            logger.info(f'Processing line {n} of {len(lines)} ({(n/len(lines)) * 100:.0f}%), '
+                        f'{len(lines)-n} left.')
+
+            if len(line) > 257:
+                logger.warning('Line is longer than 256 characters. Processing as two parts.')
+                simplified_save(line[:257])
+                simplified_save(line[257:])
             else:
-                logger.info('Downloading line...')
-                with open(f'./downloads/{safe_filename}', 'wb') as f2:
-                    f2.write(fetch(line, logger))
-                logger.info('Saved.')
+                simplified_save(line)
+
     logger.info('Work complete.')
 
 
@@ -39,6 +43,15 @@ def fetch(text, logger):
         response = requests.get(api_link.format(text=text)).content
     logger.debug('Text fetched.')
     return response
+
+def fetch_and_save(text, filename, logger):
+    if os.path.exists(f'./downloads/{filename}'):
+        logger.warning('Skipping due to existing file.')
+    else:
+        logger.info('Downloading...')
+        with open(f'./downloads/{filename}', 'wb') as f2:
+            f2.write(fetch(text, logger))
+        logger.info('Saved.')
 
 
 def get_safe_filename(s, logger):
