41e497cee9a7e64f20aa35f91c4303a7f8cf1e62
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-01-19 11:14:51 +0800

    Re-added stream handler

--- log.py
+++ log.py
@@ -10,7 +10,7 @@ else:
 
 
 def log_init(fname):
-    global file_handler, formatter, self_logger
+    global file_handler, stream_handler, self_logger
 
     log_directory = os.path.join(os.getcwd(), 'logs/')
     if not fname:
@@ -20,29 +20,19 @@ def log_init(fname):
 
     file_handler = logging.FileHandler(strftime('{}/{}_log_%y-%m-%d_%H-%M-%S.log'.format(log_directory, fname)))
     file_handler.setLevel(LOG_LEVEL)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(LOG_LEVEL)
     formatter = logging.Formatter('[%(asctime)s] %(name)s (%(levelname)s): %(message)s')
     file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
 
     public_logger = logging.getLogger('Tool-Program.{}'.format(fname))
     public_logger.addHandler(file_handler)
+    public_logger.addHandler(stream_handler)
     self_logger = logging.getLogger('Tool-Program.log')
     self_logger.addHandler(file_handler)
+    self_logger.addHandler(stream_handler)
     return public_logger
-
-
-def get_level():
-    d = {'debug': logging.DEBUG,
-         'info': logging.INFO,
-         'warning': logging.WARNING,
-         'error': logging.ERROR,
-         'critical': logging.CRITICAL}
-    try:
-        f = open('debug')
-        t = f.read()
-    except FileNotFoundError:
-        t = 'debug'
-    if t in d.keys(): return d[t]
-    else: return logging.INFO
 
 
 # Logged decorator
@@ -51,6 +41,7 @@ def logged(cls, fname):
     logger = logging.getLogger('Tool-Program.{}.{}'.format(fname, class_name))
     logger.setLevel(LOG_LEVEL)
     logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
     logger.propagate = False
     setattr(cls, 'logger', logger)
     return cls
