bb8039eb2c325a619584ab8dac0b918553213353
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-09-29 14:12:51 -0400

    chore(imperial_system_translator): Remove skeleton function and debug lines

--- imperial_system_translator/imperial_system_translator.py
+++ imperial_system_translator/imperial_system_translator.py
@@ -120,15 +120,12 @@ def get_number_segments(s: str):
     for i, char in enumerate(s):
         if char.isdigit() or char in '.':
             if start_new_segment:
-                # print('start new segment,', char, i)
                 number_segments.append(NumberSegment(char, i))
                 start_new_segment = False
             else:
-                # print('no start new segment,', char, i)
                 number_segments[-1].append(char)
         else:
             if not start_new_segment:
-                # print('end of segment', char, i)
                 start_new_segment = True
                 number_ends_at = i
     return number_segments, number_ends_at
@@ -168,7 +165,7 @@ def translate_string(number_segments: list, original_string: str, end_of_numbers
         translated_string += str(converted_segment)
         last_segment_end = index + len(segment)
     translated_string += original_string[end_of_numbers:]
-    return f'{string_without_unit(translated_string, unit_from, ureg).rstrip(" ")} {unit_to}'
+    return f'{string_without_unit(translated_string, unit_from, ureg).rstrip(" ")} {unit_to}'  # TODO: Add s if none of the numbers are plural
 
 
 def string_without_unit(s: str, unit_standard_name: str, ureg: UnitRegistry):
@@ -187,46 +184,6 @@ def string_without_unit(s: str, unit_standard_name: str, ureg: UnitRegistry):
         if s_stripped != s:
             return s_stripped
     return s
-
-
-def skeleton_main():
-    input_string = input('Enter string to be processed: ')
-    number_segments = []
-    number_ends_at = -1
-    start_new_segment = True
-    unit_from = None
-    for i, char in enumerate(input_string):
-        if char.isdigit() or char in '.':
-            if start_new_segment:
-                # print('start new segment,', char, i)
-                number_segments.append([char, i])
-                start_new_segment = False
-            else:
-                # print('no start new segment,', char, i)
-                number_segments[-1][0] += char
-        else:
-            if not start_new_segment:
-                # print('end of segment', char, i)
-                start_new_segment = True
-                number_ends_at = i
-            # else:
-                # print('not a digit', char, i)
-    if any([imp_unit in input_string for imp_unit in ('inches', 'in', 'inch', "''")]):
-        unit_from = 'inches'
-        input_string = input_string.strip('inches').strip('in').strip('inch').strip("''")
-        input_string = input_string.rstrip(' ')
-    if unit_from == 'inches':
-        translated_string = ''
-        last_segment_end = 0
-        for segment, index in number_segments:
-            # print(segment, index)
-            translated_string += input_string[last_segment_end:index]
-            # print(translated_string, 'midprocess')
-            translated_string += str(float(segment) * 2.54)
-            # print(translated_string)
-            last_segment_end = index + len(segment)
-        translated_string += input_string[number_ends_at:]
-        print(translated_string, 'cm')
 
 
 if __name__ == '__main__':
