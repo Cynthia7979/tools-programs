aa4d13d079fee799bcbd59f39122aa3ed33f62df
Cynthia7979 <cynthiawang7979@hotmail.com>
2023-12-04 22:15:09 -0500

    fix: Allow 𝐡𝐨𝐮𝐬𝐞 to modify YouTube captions

--- 𝐇𝐨𝐮𝐬𝐞_formatter/scripts/𝐇𝐨𝐮𝐬𝐞.js
+++ 𝐇𝐨𝐮𝐬𝐞_formatter/scripts/𝐇𝐨𝐮𝐬𝐞.js
@@ -4,16 +4,14 @@
     https://github.com/iamtravisw/replacer
 */
 
-console.log("⁴² \"I have trampled upon the irredeemable insanity that is the 𝐡𝐨𝐮𝐬𝐞 ... 𝐇𝐨𝐮𝐬𝐞 formatter developed by[𝘴𝘪𝘤],\" Cynthia7979, 2023, 𝒯𝑜𝑜𝓁𝓈 𝒶𝓃𝒹 𝒫𝓇𝑜𝑔𝓇𝒶𝓂𝓈, p.326.")
-
-window.onload = () => {
+const replace = () => {
     elements = document.getElementsByTagName("*")
     for (let ele of elements) {
         if (!['SCRIPT', 'STYLE'].includes(ele.tagName)) {
             for (let node of ele.childNodes) {
                 if (node.nodeType === 3) {
                     let text = node.nodeValue
-                    let replacedText = text.replace('House', '𝐇𝐨𝐮𝐬𝐞').replace('house', '𝐡𝐨𝐮𝐬𝐞')
+                    let replacedText = text.replace('Houses', '𝐇𝐨𝐮𝐬𝐞𝐬').replace('House', '𝐇𝐨𝐮𝐬𝐞').replace('houses', '𝐡𝐨𝐮𝐬𝐞𝐬').replace('house', '𝐡𝐨𝐮𝐬𝐞')
                     if (replacedText !== text) {
                         ele.replaceChild(document.createTextNode(replacedText), node)
                     }
@@ -22,3 +20,15 @@ window.onload = () => {
         }
     }
 }
+
+// Dynamically alter YouTube captions
+const observer = new MutationObserver(replace);
+const ytbCaptionWindowContainer = document.getElementById("ytp-caption-window-container");
+observer.observe(ytbCaptionWindowContainer, {
+    childList: true,
+    subtree: true,
+});
+
+window.onload = replace;
+
+console.log("⁴² \"I have trampled upon the irredeemable insanity that is the 𝐡𝐨𝐮𝐬𝐞 ... 𝐇𝐨𝐮𝐬𝐞 formatter developed by[𝘴𝘪𝘤],\" Cynthia7979, 2023, 𝒯𝑜𝑜𝓁𝓈 𝒶𝓃𝒹 𝒫𝓇𝑜𝑔𝓇𝒶𝓂𝓈, p.326.")
